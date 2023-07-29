10d343b63247b00257677e9faa07bef694592b34
tyVespA <migliavacca.marco08@gmail.com>
2023-07-29 15:14:39 +0200

    Create and style Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+import MainButton from "./MainButton";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <a href="#">
+        <img src="../../public/images/logo.svg" alt="logo" height={24} />
+      </a>
+      <nav className={styles.navigation}>
+        <a href="#">Pricing</a>
+        <a href="#">Product</a>
+        <a href="#">About us</a>
+        <a href="#">Careers</a>
+        <a href="#">Community</a>
+      </nav>
+      <MainButton />
+    </header>
+  );
+}
+
+export default Header;
