4d30d5468f4829d02f3ebec1841c800d0f348eb3
tyVespA <migliavacca.marco08@gmail.com>
2023-07-29 15:14:31 +0200

    Create and style MainButton

---
+++ src/components/MainButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/MainButton.module.css";
+
+function MainButton() {
+  return <button className={styles.mainButton}>Get Started</button>;
+}
+
+export default MainButton;
