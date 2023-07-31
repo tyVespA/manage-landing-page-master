ecee67d0f4821dec4ed244f20ab533e97ec8cddc
tyVespA <migliavacca.marco08@gmail.com>
2023-07-31 14:16:52 +0200

    Add responsiveness

--- src/components/BulletPoint.jsx
+++ src/components/BulletPoint.jsx
@@ -3,10 +3,11 @@ import styles from "../styles/BulletPoint.module.css";
 
 function BulletPoint({ number, title, text }) {
   return (
-    <div className={styles.bulletPointContainer}>
-      <p className={styles.bulletPoint}>{number}</p>
-      <h3>{title}</h3>
-      <div></div>
+    <div>
+      <div className={styles.title}>
+        <p className={styles.bulletPoint}>{number}</p>
+        <h3>{title}</h3>
+      </div>
       <p className={styles.text}>{text}</p>
     </div>
   );
